90f162ba5978681d2f0ce024e17913c75d6a9af1
Pabanthapa91 <aanayabthapa@gmail.com>
2020-10-31 11:55:13 -0400

    first commit

---
+++ HalloweenProject/src/main/java/halloween.java
@@ -0,0 +1,7 @@
+public class halloween {
+    public static void main(String[] args) {
+
+
+        System.out.println("THIS IS A HALLOWEEN PARTY");
+    }
+}
---
+++ HalloweenProject/src/test/java/Halloweentest.java
@@ -0,0 +1,7 @@
+public class Halloweentest {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is a halloween test");
+    }
+}
